fb252353dcd07d2006d00bec1059f7b0e0d60ce1
luizrosalba <luizrosalba@gmail.com>
2022-01-25 08:10:08 -0300

    configs done

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,6 +1,15 @@
+const path = require('path'); /// nodejs package
+
 const config = { 
     /// relative to root dir
-    entry: './src/index.js'
+    entry: './src/index.js', /// relative file reference
+    /// where to save files bundled
+    output: {
+        path: path.resolve(__dirname, 'build'), 
+        /// absolute file reference using path module
+        /// path.resolve makes path valid for any OS windows, mac, linux...
+        filename: 'bundle.js' // any name is valid
+    }
 };
 
 module.exports = config;
